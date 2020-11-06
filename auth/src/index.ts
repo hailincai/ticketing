78a47432add12b39ce32a0cb074a81fb876c8696
Hailin Cai <hailin.cai@endeavorstreaming.com>
2020-11-06 14:39:58 -0500

    test auth build

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('startup auth service...')
+  console.log('startup auth service..')
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
